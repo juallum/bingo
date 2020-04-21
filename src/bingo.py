15587eacf06bea1cfcf74387e1f7451bd6a0670e
juan <juanguida02@gmail.com>
2020-04-21 14:35:21 -0300

    tarea_1_hecha

--- src/bingo.py
+++ src/bingo.py
@@ -1,9 +1,16 @@
 def carton():
 	carton = (
-		(1,0,0,1,1,0,1,0,1),
-		(0,1,1,1,0,1,1,1,1),
+		(1,0,1,1,1,0,1,0,1),
+		(0,1,0,1,0,1,1,1,1),
 		(0,1,0,0,1,0,0,1,0)
 	)
 	return carton
 
-print(carton)
+def columna(carton, nmro_columna):
+	return(
+		carton[0][nmro_columna],
+		carton[1][nmro_columna],
+		carton[2][nmro_columna]
+	)
+
+print (columna(carton(),1))
--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,5 +1,5 @@
 from src.bingo import carton
-
+from src.bingo import columna
 def test_menos_de_quince():
 	menos_carton = carton()
 	contador = 0
@@ -18,3 +18,15 @@ def test_mas_de_quince():
 			
 	assert contador <= 15
 
+def test_columnas():
+	test_carton = carton()
+	contador = 0
+	bandera_test = 0
+	while contador < 9:
+		if ((columna(test_carton, contador)) == (0, 0, 0)):
+			bandera_test = 1
+			break
+		
+		contador += 1
+	
+	assert bandera_test == 0;
